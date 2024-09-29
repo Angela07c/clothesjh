9597012f301613d21fbc9ccb9f6aa4d7fcc6d09c
Duban Lopez <nlopezr6@ucentral.edu.co>
2024-09-28 20:52:29 -0500

    Creacion del modelo de Order y  cart

---
+++ src/models/Cart.model.js
@@ -0,0 +1,46 @@
+const mongoose = require( 'mongoose' );
+
+const CartSchema = new mongoose.Schema ({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    },
+
+    products: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'products'
+            },
+
+            quantity: {
+                type: Number,
+                required: true,
+                default: 1,
+                min: 1
+            },
+
+
+        }
+    ],
+
+    status: {
+        type: String,
+        enum: ['pending' , 'finished' , 'delivered' , 'refurbished'],
+        default: 'pending'
+    }
+
+
+});
+
+
+
+
+
+const CartModel = mongoose.model(
+    'carts',
+    CartSchema
+);
+
+module.exports = CartModel;
